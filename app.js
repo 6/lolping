62e21bd4054244134ce734a5ec4413827e5ac6f6
Peter Graham <peterghm@gmail.com>
2016-02-28 19:38:42 -0500

    add a Quit option

--- app.js
+++ app.js
@@ -8,15 +8,16 @@ var imagePath = function(filename) {
   return path.join(__dirname, 'images', filename);
 };
 
+var quit = function() {
+  app.quit();
+};
+
 var appIcon = null;
 app.dock.hide();
 app.on('ready', function(){
   appIcon = new Tray(imagePath('IconTemplate.png'));
   var contextMenu = Menu.buildFromTemplate([
-    { label: 'Item1', type: 'radio' },
-    { label: 'Item2', type: 'radio' },
-    { label: 'Item3', type: 'radio', checked: true },
-    { label: 'Item4', type: 'radio' }
+    {label: 'Quit', type: 'normal', click: quit}
   ]);
   appIcon.setToolTip('This is my application.');
   appIcon.setContextMenu(contextMenu);
