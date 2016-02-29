8687b52aa168410aadcecc5ae2ce6ee52cebf0e5
Peter Graham <peterghm@gmail.com>
2016-02-28 20:22:33 -0500

    add more server IPs, set current one as checked

--- app.js
+++ app.js
@@ -3,14 +3,23 @@ const app = electron.app;
 const Menu = electron.Menu;
 const Tray = electron.Tray;
 var path = require('path');
+var _ = require('lodash');
 
 var trayApp = null;
 var pingMenuItem = null;
-var ips = {
+
+// IPs from https://support.riotgames.com/hc/en-us/articles/201752674-Network-System-and-League-of-Legends-Logs
+var servers = {
   NA: "104.160.131.1",
-  EUW: "185.40.65.1"
+  EUW: "185.40.65.1",
+  EUNE: "31.186.224.42",
+  OCE: "103.240.227.5",
+  BR: "8.23.24.4",
+  LAN: "66.151.33.33",
+  LAS: "138.0.12.100",
+  TR: "31.186.226.34"
 };
-var currentIp = ips.NA;
+var currentIp = servers.NA;
 
 var imagePath = function(filename) {
   return path.join(__dirname, 'images', filename);
@@ -20,8 +29,8 @@ var quit = function() {
   app.quit();
 };
 
-var changeIp = function(serverLocation) {
-  var newIp = ips[serverLocation];
+var changeServer = function(server) {
+  var newIp = servers[server];
   if (newIp === currentIp) {
     return;
   }
@@ -33,18 +42,22 @@ app.dock.hide();
 app.on('ready', function(){
   trayApp = new Tray(imagePath('IconTemplate.png'));
   var contextMenuTemplate = [];
-  for(var serverLocation in ips) {
-    var onClick = (function(serverLocation) {
-      return function(){ changeIp(serverLocation); }
-    })(serverLocation);
+  for(var server in servers) {
+    var onClick = (function(server) {
+      return function(){ changeServer(server); }
+    })(server);
     contextMenuTemplate.push({
-      label: serverLocation,
+      label: server,
       type: 'radio',
       click: onClick
     });
   }
   contextMenuTemplate.push({type: 'separator'});
   contextMenuTemplate.push({label: 'Quit', type: 'normal', click: quit});
+  var currentServerItemIndex = _.findIndex(contextMenuTemplate, function(item) {
+    return servers[item.label] === currentIp;
+  });
+  contextMenuTemplate[currentServerItemIndex].checked = true;
   var contextMenu = Menu.buildFromTemplate(contextMenuTemplate);
 
   trayApp.setToolTip('This is my application.');
